5c947189a43b4a1d0b0e92444184b89bda9bcacb
Maxime DUFOUR <maxime-dufour@outlook.fr>
2015-05-29 16:03:19 +0200

    Creation du tableau à partir de la String MAP

--- src/pack/fonctions.java
+++ src/pack/fonctions.java
@@ -1,33 +1,66 @@
 package pack;
 
+import java.util.ArrayList;
+
 public class fonctions {
 
-	public static int LigneMap(String[] s)
-	{
-		return s.length;
-	}
-	
-	public static int ColMap(String[] s)
-	{
-		return s.length;
-	}
-	
-	public static String[] splitLigne(String s)
+	public static int LigneMap(String s)
 	{
 		String inter = s.replace("|","-");
 		String[] truc = inter.split("-");
 		
-		/*for (int i = 0; i<truc.length; i++)
-			System.out.println(truc[i]);
-			*/
-		return truc;
-		
+		return truc.length;
 	}
 	
-	public static String[] splitCol(String tab)
+	public static int ColMap(String s)
 	{
-		String[] truc;
-		truc = tab.split(":");
-		return truc;
+		String[] inter = s.replace("|","-").split("-");
+		String[] truc = inter[0].split(":");
+		return truc.length;
 	}
+	
+	public static String[][] tableau (String s, int ligne, int col)
+	{
+		String [][] tableau = new String [ligne][col];
+		
+		for (int i = 0; i<ligne; i++) {
+			for (int j = 0; j<col; j++) {
+				String[] inter = s.replace("|","-").split("-");
+				String[] truc = inter[i].split(":");
+				tableau [i][j] = truc [j];
+			}
+		}
+		return tableau;	
+	}
+	
+	public static void tableau_toString (String [][] tableau) {
+		
+		for (int i = 0; i<tableau.length; i++) {
+			for (int j = 0; j<tableau[0].length; j++) {
+				System.out.print(tableau[i][j] + " | ");
+			}
+			System.out.println("");
+		}
+	}
+	
+	public static void isOccupied (ArrayList<String> taken, String nbcase)
+	{
+		taken.add(nbcase);//EX:9B1
+		String[] all=nbcase.split("");
+		String[] alpha={"Z","A","B","C","D","E","F","G","H","I","J","Y"};
+		int i=0;
+		
+		while(!(all[2].equals(alpha[i])))
+		{
+			i++;
+		}
+
+		String case1=all[1]+alpha[i-1]+(Integer.parseInt(all[3])+1);
+		taken.add(case1);//EX:9A2
+		String case2=(Integer.parseInt(all[1])-1)+alpha[i-1]+(Integer.parseInt(all[3])+2);
+		taken.add(case2);//EX:8A3
+		String case3=(Integer.parseInt(all[1])-1)+alpha[i]+(Integer.parseInt(all[3])+3);
+		taken.add(case3);//EX:8B4
+	}
+
 }
--- src/pack/test.java
+++ src/pack/test.java
@@ -9,10 +9,12 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String val = "25:26:27:45|54:12:13:14|67:34:35:36";
-		String[] truc = fonctions.splitLigne(val);
-		String [] machin = fonctions.splitCol(truc[0]);
-		int col = fonctions.ColMap(machin);
-		int row = fonctions.LigneMap(truc);
+		int col = fonctions.ColMap(val);
+		int row = fonctions.LigneMap(val);
+		String [][] tableau = fonctions.tableau(val, row, col);
+		
+		fonctions.tableau_toString(tableau);
+		
 		System.out.println("Nb row:" + row);
 		System.out.println("Nb col:" + col);
 	}
